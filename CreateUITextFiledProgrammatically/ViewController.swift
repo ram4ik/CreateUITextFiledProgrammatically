843421ed1765477fce2e3b7065754cfe76aa0e26
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-10-12 11:13:27 +0300

    init

--- CreateUITextFiledProgrammatically/ViewController.swift
+++ CreateUITextFiledProgrammatically/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let txtUserName = UITextField(frame: CGRect(x: 10.0, y: 100.0, width: UIScreen.main.bounds.size.width - 20.0, height: 50.0))
+        txtUserName.backgroundColor = .lightGray
+        txtUserName.borderStyle = .line
+        txtUserName.keyboardAppearance = .light
+        txtUserName.keyboardType = .default
+        txtUserName.placeholder = "Enter user name"
+        txtUserName.font = UIFont.systemFont(ofSize: 15.0)
+        txtUserName.delegate = self
+        
+        self.view.addSubview(txtUserName)
     }
-
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
